2596b1590ff08bf8bb36aa211cd86e8b9144ae7b
ARAJO <dddd2511u@gmail.com>
2023-05-25 18:56:46 +0900

    33

--- src/pollsWithoutMethod.java
+++ src/pollsWithoutMethod.java
@@ -3,29 +3,47 @@ import java.util.Scanner;
 public class pollsWithoutMethod {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        String answer = "";   
-        String[] answers = { "", ""};
 
-        System.out.println("1. 문항");
-        System.out.println("(1) 답항"+"(2) 답항"+"(3)답항"+"(4)답항" );
-        System.out.print ("답하기:");
-        answers[0] = myObj. nextLine();
-
-        
-        System.out.println("2. 문항");
-        System.out.println("(1) 답항"+"(2) 답항"+"(3)답항"+"(4)답항");
-        System.out.print ("답하기:");
-        answers[1]= myObj. nextLine();
-        
-        for (int first=0; first < answers.length; first++){
-            System.out.print(answers[first]+",");
-         
-    }
+        System.out.println("이름을 입력하세요");
+        System.out.print(" 이름)  ");
+        String name = myObj.nextLine();
         System.out.println();
-        // return 0;
 
-}      
 
-}       
-       
-     
+        String[] answers = {"","","",""};
+        String[][] polls = 
+            {{"1.교수는 수업 전 강의 목표를 명확히 제시하였습니까? "}, 
+             
+             {"2.강의의 내용은 체계적이고 성의있게 구성되었는가? "}, 
+             
+             {"3.강의의 내용은 체계적이고 성의있게 구성되었는가? "}, 
+            
+             {"4. 강의 진행 속도는 적절하였는가?"},
+          
+            };
+
+            
+            for (int second=0; second < polls.length; second=second+1){
+                System.out.println(polls[second][0]);
+                // 답항 출력 본인이 해 보기
+    
+                System.out.println("(1)전혀 아니다. (2)아니다. (3)그렇다. (4)매우그렇다.");
+                System.out.println();
+                System.out.print("답하기 : ");
+                answers[second] = myObj.nextLine();
+                
+                System.out.println();
+            }
+    
+            System.out.println("---------------설문 종료---------------");
+            System.out.println();
+            System.out.println("---------------설문 결과---------------");
+
+
+            for (int first=0; first < answers.length; first=first+1) {
+                System.out.print(answers[first]+", ");
+            }
+            System.out.println();
+            // return 0;
+        }
+    }
---
+++ src/re.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class re {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("이름을 입력하세요");
+        System.out.print(" 이름)  ");
+        String name = scanner.nextLine();
+        System.out.println();
+
+        
+        String[] questions = {
+            "1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?",
+            "2. 강의의 내용은 체계적이고 성의있게 구성되었는가?",
+            "3. 교수는 강의 내용에 대해 전문적 지식이 있었는가?",
+            "4. 강의 진행 속도는 적절하였는가?"
+        };
+
+
+        int[] answers = new int[questions.length];
+
+        for (int first = 0; first < questions.length; first++) {
+            System.out.println(questions[first]);
+            System.out.println("(1)전혀 아니다. (2)아니다. (3)그렇다. (4)매우그렇다.");
+            System.out.println();
+
+            while (true) {
+                System.out.print("답) ");
+                int answer = scanner.nextInt();
+
+                if (answer >= 1 && answer <= 4) {
+                    answers[first] = answer;
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+
+        System.out.println("---------------설문 종료---------------");
+        System.out.println();
+        System.out.println("---------------설문 결과---------------");
+        
+
+        for (int first = 0; first < answers.length; first++) {
+            if (first != 0) {
+                System.out.print(", ");
+            }
+            System.out.printf("%d. (%s)", first + 1, answers[first]);
+        }
+    }
+}
