2d0afafb06ba2033adfbe29943eb6136eb207173
ARAJO <dddd2511u@gmail.com>
2023-05-31 13:26:16 +0900

    first commit

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -1,25 +1,30 @@
-import java. util.Scanner;
+import java.util.Scanner;
+
 public class MaxMins {
-public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
 
-    int first =  0;
-    int second = 0;
-    first = myObj.nextInt();
-    second = myObj.nextInt();
-    int min = Math. min (first, second);
-
-    System.out.println(min);
-    try {
-           
-    } catch (Exception e) {
-        // TODO: handle exception
+    public static int getMin(int first, int second) {
+        return Math.min(first, second);
     }
-    // return 0;
-  
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        try {
+
+            int first = 0;
+            int second = 0;
+            first = myObj.nextInt();
+            second = myObj.nextInt();
+
+            int min = getMin(first, second);
+
+            System.out.println(min);
+
+        } catch (Exception e) {
+
+        }
+        // return 0;
+
     }
 
 }
-
-
-
